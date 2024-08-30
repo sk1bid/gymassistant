07feaa0e11145c916eeda369fe4701737b7fbef4
Иванов Артём Михайлович <is342s12@csc.sibsutis.ru>
2024-08-30 15:59:36 +0700

    Change method of parsing

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
 
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode='HTML')
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode='HTML'))
 
 dp = Dispatcher()
 
